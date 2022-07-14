067a7d3388bf651715cae68ae0385103bec530df
Lee JaeHyeok <dhfif718@gmail.com>
2022-07-14 23:18:42 +0900

    A_개발 1차진행

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 #메인 프로그램 개발시작
 
 #################
+
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+#aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+
+
+
